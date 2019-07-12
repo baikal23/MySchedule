dfb050d6df77b5f436f5f3b285f9b39afcdc94f0
Sue Kohler <skohler00@gmail.com>
2019-07-12 19:08:20 -0400

    can do series of weeks, and blank schedule for current week

--- MySchedule/ViewControllers/ReportManagerViewController.swift
+++ MySchedule/ViewControllers/ReportManagerViewController.swift
@@ -15,6 +15,7 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
     var currentStartTime = Date()
     var currentEndTime = Date()
     var reportName = ""
+    var reporter:ReportGenerator!
     
     @IBOutlet weak var startDatePicker: UIPickerView!
     @IBOutlet weak var endDatePicker: UIPickerView!
@@ -22,6 +23,7 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        reporter = ReportGenerator()
         self.startDatePicker.delegate = self
         self.startDatePicker.dataSource = self
         endDatePicker.delegate = self
@@ -35,11 +37,30 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
     @IBAction func makeReportPushed(_ sender: Any) {
         let weeksInRange = WeekArray.getWeeksInRangeFrom(currentStartTime, endDate: currentEndTime)
         print("Got the schedules")
+        reportName = "WeeklySchedule"
+        reporter.setupPDFDocumentNamed(name: reportName, width: pdfPageWidth, height: pdfPageHeight)
         for item in weeksInRange {
-            self.makePDFreportFor(week:item)
+            self.makePDFreportFor(week:item, withParticipants: true)
         }
+        reporter.finishPDF()
+        let nextViewController = PDFViewController()
+        nextViewController.reportName = reportName
+        nextViewController.userName = ""
+        self.navigationController?.pushViewController(nextViewController, animated: true)
     }
     
+    @IBAction func blankSchedulePushed(_ sender: Any) {
+        let currentWeek = Week.getWeekOf(currentStartTime)
+        reportName = "BlankSchedule"
+        reporter.setupPDFDocumentNamed(name: reportName, width: pdfPageWidth, height: pdfPageHeight)
+        self.makePDFreportFor(week:currentWeek, withParticipants: false)
+        reporter.finishPDF()
+        let nextViewController = PDFViewController()
+        nextViewController.reportName = reportName
+        nextViewController.userName = ""
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+        
+    }
     @IBAction func participantReportPushed(_ sender: Any) {
         var activitiesForReport:[String] = []
         var activitySummary = (name:"name", count:0)
@@ -76,13 +97,11 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
         print("Got the schedule")
     }
     
-    func makePDFreportFor(week:Week){
+    func makePDFreportFor(week:Week, withParticipants:Bool){
         let dateString = CalendarDays.stringFromDate(week.dateStamp)
         let reportTitle = "Schedule for Week of " + dateString
-        let reporter = ReportGenerator()
+        //let reporter = ReportGenerator()
         let pageSize = CGSize(width: pdfPageWidth, height: pdfPageHeight)
-        reportName = "WeeklySchedule"
-        reporter.setupPDFDocumentNamed(name: reportName, width: pdfPageWidth, height: pdfPageHeight)
         reporter.beginPDFPage()
         let titleTextRect = reporter.addText(text: reportTitle, frame: CGRect(x: pdfPadding, y: pdfPadding, width: (pageSize.width) - 2 * pdfPadding, height: pdfTitleBoxHeight), fontSize: pdfTitleFont)
         var verticalDistance = pdfPadding + titleTextRect.size.height + pdfPadding
@@ -93,7 +112,12 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
             let headingRect = reporter.addText(text: block.scheduleTime, frame: CGRect(x: pdfPadding, y: verticalDistance, width: (pageSize.width) - 2 * pdfPadding, height: pdfHeadingBoxHeight), fontSize: pdfHeadingFont)
             verticalDistance = verticalDistance + headingRect.size.height + pdfPadding
             for activity in block.activityArray {
-                let activityRect = reporter.addText(text: activity.activityName, frame: CGRect(x: pdfPadding, y: verticalDistance, width: (pageSize.width) - 2 * pdfPadding, height: pdfTextLineBoxHeight), fontSize: pdfActivityFont)
+                var activityText = activity.activityName
+                if (withParticipants) {
+                    let joined = activity.participants.joined(separator: ", ")
+                    activityText = activityText + ":  " + joined
+                }
+                let activityRect = reporter.addText(text: activityText, frame: CGRect(x: pdfPadding, y: verticalDistance, width: (pageSize.width) - 2 * pdfPadding, height: pdfTextLineBoxHeight), fontSize: pdfActivityFont)
                 verticalDistance = verticalDistance + activityRect.size.height + pdfPadding
                 if (verticalDistance > pdfPageHeight - pdfBottomMargin) {
                     reporter.beginPDFPage()
@@ -101,12 +125,6 @@ class ReportManagerViewController: UIViewController, UIPickerViewDelegate, UIPic
                 }
             }
         }
-        reporter.finishPDF()
-        let nextViewController = PDFViewController()
-        nextViewController.reportName = reportName
-        nextViewController.userName = ""
-    self.navigationController?.pushViewController(nextViewController, animated: true)
-        
     }
     // MARK: - Picker Views
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
